e7449daff545dbf7b95833436423212a59dc4810
Donnie Conner <doniconner@gmail.com>
2012-12-13 11:21:17 -0800

    Added more plugins and mobile redirect options

--- all/index.js
+++ all/index.js
@@ -20,18 +20,16 @@ Generator.prototype.askFor = function askFor(argument){
     self = this;
 
   // Welcome message
-  var welcome =
-  '\n     _-----_' +
-  '\n    |       |' +
-  '\n    |'+'--(o)--'.red+'|   .--------------------------.' +
-  '\n   `---------´  |    '+'Welcome to Yeoman,'.yellow.bold+'    |' +
-  '\n    '+'( '.yellow+'_'+'´U`'.yellow+'_'+' )'.yellow+'   |   '+'ladies and gentlemen!'.yellow.bold+'  |' +
-  '\n    /___A___\\   \'__________________________\'' +
-  '\n     |  ~  |'.yellow +
-  '\n   __'+'\'.___.\''.yellow+'__' +
-  '\n ´   '+'`  |'.red+'° '+'´ Y'.red+' `\n';
 
-  console.log(welcome);
+  var Welcome = 
+  '\n __        ___ _     _  __ _'.red+          
+  '\n \\ \\      / (_) | __| |/ _(_)_ __ ___ '.red +
+  "\n  \\ \\ /\\ / /| | |/ _` | |_| | '__/ _ \\ ".red +
+  "\n   \\ V  V / | | | (_| |  _| | | |  __/".red +
+  "\n    \\_/\\_/  |_|_|\\__,_|_| |_|_|  \\___|".red+'\n';
+                                     
+
+  console.log(Welcome);
   console.log("I'm here to help you start your new Pages template!");
 
   var prompts = [{
@@ -40,20 +38,30 @@ Generator.prototype.askFor = function askFor(argument){
     default: 'Y/n',
     warning: 'This will create mobile.liquid and mobile.css'
   },{
+    name: 'redirect',
+    message: 'Would you like to use the canvas page as a mobile redirect?',
+    default: 'y/N',
+    warning: 'This will create fbcanvas.liquid with the redirect script'
+  },{
     name: 'microsite',
     message: 'Are you developing for microsite?',
     default: 'y/N',
     warning: 'This will create microsite.liquid and microsite.css'
   },{
-    name: 'fbcanvas',
-    message: 'Are you developing a canvas page?',
-    default: 'y/N',
-    warning: 'This will create fbcanvas.liquid and fbcanvas.css'
-  },{
     name: 'gallery',
     message: 'Would you like to include a gallery?',
     default: 'y/N',
     warning: 'This will generate a gallery plugin'
+  },{
+    name: 'sweepstakes',
+    message: 'Would you like to add a sweepstakes?',
+    default: 'y/N',
+    warning: 'This will generate a sweepstakes plugin'
+  },{
+    name: 'voting',
+    message: 'Do you want to include a voting gallery?',
+    default: 'y/N',
+    warning: 'This will generate a voting gallery'
   }];
 
   this.prompt(prompts, function(e, props){
@@ -61,8 +69,10 @@ Generator.prototype.askFor = function askFor(argument){
 
     self.mobile = (/y/i).test(props.mobile);
     self.microsite = (/y/i).test(props.microsite);
-    self.fbcanvas = (/y/i).test(props.fbcanvas);
+    self.redirect = (/y/i).test(props.redirect);
     self.gallery = (/y/i).test(props.gallery);
+    self.sweepstakes = (/y/i).test(props.sweepstakes);
+    self.voting = (/y/i).test(props.voting)
 
     cb();
   });
@@ -86,20 +96,29 @@ Generator.prototype.createTemplate = function(){
     this.template(path.join('assets', 'microsite.css'), path.join('assets', 'microsite.css'));
   };
 
-  if (this.fbcanvas){
-    this.template('fbcanvas.liquid', 'fbcanvas.liquid', data);
-    this.template(path.join('assets', 'fbcanvas.css'), path.join('assets', 'fbcanvas.css'));
+  if (this.redirect){
+    this.copy('fbcanvas.liquid', 'fbcanvas.liquid');
   };
 };
 
 Generator.prototype.buildData = function(){
   var data = {
-    gallery: false
+    gallery: false,
+    sweepstakes: false,
+    voting: false
   };
 
   if (this.gallery){
     data.gallery = true;
   };
+
+  if (this.sweepstakes){
+    data.sweepstakes = true;
+  };
+
+  if (this.voting){
+    data.voting = true;
+  }
 
   return data;
 }
